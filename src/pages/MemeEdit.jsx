df02687d8b33330dfe001afafc2e9863b3ccf29f
samir-27 <sumrasamir27@gmail.com>
2024-01-20 11:41:49 +0530

    add responsive ness and fix bug

--- src/pages/MemeEdit.jsx
+++ src/pages/MemeEdit.jsx
@@ -29,21 +29,22 @@ const MemeEdit = () => {
   }, [id]);
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center bg-gray-100 min-h-screen">
       <div className="mt-12 w-full max-w-screen-lg">
         {meme && (
-          <div className="w-full mt-8 sm:mt-12">
-            <img
-              ref={memeRef}
-              className="max-w-full h-auto object-contain mx-auto border-2 border-gray-600 mb-4"
-              src={meme.url}
-              alt={meme.name}
-            />
-            <div className="sm:flex flex-row sm:justify-center sm:items-center sm:space-y-0 flex flex-col">
+          <>
+            <div ref={memeRef} className="w-full px-5 mt-8 sm:mt-12">
+              <img
+                className="lg:max-w-128 md:w-128 max-h-128 w-auto h-auto object-contain mx-auto border-2 border-gray-600 mb-4"
+                src={meme.url}
+                alt={meme.name}
+              />
               {Array(count).fill(0).map((e, index) => (
                 <Text key={index} />
               ))}
-              <div className="sm:flex sm:flex-col sm:items-center sm:space-y-2 sm:mt-4">
+            </div>
+            <div className="sm:flex sm:flex-col sm:items-center sm:space-y-2 sm:mt-4">
+              <div className="sm:flex flex-row sm:justify-center sm:items-center sm:space-y-0 flex flex-col">
                 <button
                   onClick={addText}
                   className="bg-gray-600 px-4 py-2 rounded text-lg text-white m-2"
@@ -60,7 +61,8 @@ const MemeEdit = () => {
                 </button>
               </div>
             </div>
-          </div>
+
+          </>
         )}
       </div>
     </div>
